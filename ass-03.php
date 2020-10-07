4bf18372ae18ffc1e8f9d3cf3fe04788cd906642
pacharacamt <pachara.t@cmu.ac.th>
2020-10-07 03:44:22 +0000

    assignment 3 passed.

--- ass-03.php
+++ ass-03.php
@@ -10,11 +10,21 @@ for($i=0;;$i++){
     }
 }fclose($f);
 $ps=implode(" ",$p);
-$pa=explode(" ",$ps);
-for($j=0;$j<count($pa);$j++){
-    if(preg_match("/(?=.*[\(])(?=.*[\)])/",$pa[$j])&&(strtoupper($pa[$j])===$pa[$j])){
-       $ea[]=str_ireplace(["(",")",".",","],"",$pa[$j]);
-    }
-}for($k=0;$k<count($ea);$k++){
+/**
+ * Your solution looks good but it cannot deal with number,
+ * e.g. (2020) then I will provide you with another solution
+ * with help of sub-pattern matched of preg_match_all().
+ */
+// $pa=explode(" ",$ps);
+// for($j=0;$j<count($pa);$j++){
+//     if(preg_match("/(?=.*[\(])(?=.*[\)])/",$pa[$j])&&(strtoupper($pa[$j])===$pa[$j])){
+//        $ea[]=str_ireplace(["(",")",".",","],"",$pa[$j]);
+//     }
+// }
+
+preg_match_all("/\(([A-Z]+)\)/", $ps, $result);
+$ea = $result[1];
+
+for($k=0;$k<count($ea);$k++){
     echo$ea[$k],"\n";
 }
