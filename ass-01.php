c3cc2d9779657436d12aa3c54e88d8ea24d9c239
pacharacamt <pachara.t@cmu.ac.th>
2020-10-07 03:08:17 +0000

    assignment 1 passed.

--- ass-01.php
+++ ass-01.php
@@ -6,10 +6,14 @@ $f=fopen($_SERVER['argv'][1],'r');
 fscanf($f,"%d",$n);
 for($i=0;$i<$n;$i++){
     $pw[$i]=trim(fgets($f));
-    $ltr[$i]=preg_match_all("/[a-zA-Z]/",$pw[$i]);
-    $num[$i]=preg_match_all("/\d/",$pw[$i]);
-    $smb[$i]=preg_match_all("/[\$@&]/",$pw[$i]);
-    if((preg_match("/(?=.*[a-zA-Z])(?=.*\d)(?=.*[\$@&])/",$pw[$i]))&&($ltr[$i]>=2)&&($num[$i]>=2)&&($smb[$i]>=2)&&(strpos($pw[$i]," ")===false)){
+    /**
+     * If you only use lookahead it will be simpler.
+     */
+    //$ltr[$i]=preg_match_all("/[a-zA-Z]/",$pw[$i]);
+    //$num[$i]=preg_match_all("/\d/",$pw[$i]);
+    //$smb[$i]=preg_match_all("/[\$@&]/",$pw[$i]);
+    //if((preg_match("/(?=.*[a-zA-Z])(?=.*\d)(?=.*[\$@&])/",$pw[$i]))&&($ltr[$i]>=2)&&($num[$i]>=2)&&($smb[$i]>=2)&&(strpos($pw[$i]," ")===false)){
+    if(preg_match("/(?=(.*[a-zA-Z]){2})(?=(.*\d){2})(?=(.*[\$@&]){2})(?=.{8})(?!.*\s)/",$pw[$i])){
         $s[]=$pw[$i];
     }else{
         $ns[]=$pw[$i];
